73156c8a20f8e8143850349c8fd0ed98f2655eaa
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2019-04-29 17:13:16 +0200

    removed spurious scripts

--- bin/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./deployNagini.sh
-./deployRunner.sh
--- bin/deployRunner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /home/developer/source/arp-plugin/
-sbt assembly
-cp target/scala-2.11/arp-plugin.jar ../viper-runner/fatjars/silicon-base.jar
-cd /home/developer/source/arp-plugin-carbon/
-sbt assembly
-cp target/scala-2.11/arp-plugin.jar ../viper-runner/fatjars/carbon-base.jar
